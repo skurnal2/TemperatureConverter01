7ca6ffae5ee3b667d01a99f9bbb78030b3c5d76e
Siddharth Kurnal <49497542+skurnal2@users.noreply.github.com>
2020-01-13 19:38:59 -0700

    TemperatureConverter01

--- TemperatureConverter01/ViewController.swift
+++ TemperatureConverter01/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var inputTemp: UITextField!
+    
+   
+    @IBAction func convertTemperature(_ sender: UIButton) {
+        let button = sender as UIButton
+        if button.tag == 1 {
+            let fahrenheit = Double(inputTemp.text!)!
+            let celcius = tempConverter.fahrenheitToCelcius(fahrenheit)
+            OutputLabel.text = "\(celcius)°C"
+        } else if button.tag == 2 {
+            let celcius = Double(inputTemp.text!)!
+            let fahrenheit = tempConverter.celciusToFahrenheit(celcius)
+            OutputLabel.text = "\(fahrenheit)°F"
+        }
+    }
+    
+    @IBOutlet weak var OutputLabel: UILabel!
+    
+    let tempConverter = TemperatureConverter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let celcius = 0.0
+        let fahrenheit = tempConverter.celciusToFahrenheit(celcius)
+        OutputLabel.text = "\(fahrenheit)°F"
+        //OR this way
+        //OutputLabel.text = String(fahrenheit)
     }
-
-
 }
 
